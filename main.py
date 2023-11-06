ea033bf4125668400f817a4678127112673a1b61
Lahiru Pathirage <lpsandaruwan@gmail.com>
2023-11-06 23:13:42 +0530

    updated to gen 2

--- main.py
+++ main.py
@@ -1,16 +1,8 @@
 import base64
+import functions_framework
 
-
-def main(event, context):
-    """Background Cloud Function to be triggered by Pub/Sub.
-    Args:
-         event (dict):  The dictionary with data specific to this type of
-         event. The `data` field contains the PubsubMessage message. The
-         `attributes` field will contain custom attributes if there are any.
-         context (google.cloud.functions.Context): The Cloud Functions event
-         metadata. The `event_id` field contains the Pub/Sub message ID. The
-         `timestamp` field contains the publish time.
-    """
-    context_event_id = context.event_id
-    context_timestamp = context.timestamp
-    event_data =  base64.b64decode(event['data']).decode('utf-8')
+# Triggered from a message on a Cloud Pub/Sub topic.
+@functions_framework.cloud_event
+def hello_pubsub(cloud_event):
+    # Print out the data from Pub/Sub, to prove that it worked
+    print(base64.b64decode(cloud_event.data["message"]["data"]))
